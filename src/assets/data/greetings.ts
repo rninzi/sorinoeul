b8b339cd5668da2f11093db0f3108efe8206b9cd
mywhzy <cmj2393a@naver.com>
2024-01-03 14:24:31 +0900

    feat: 인사말 컴포넌트 내부 문자열 data 추가

---
+++ src/assets/data/greetings.ts
@@ -0,0 +1,6 @@
+export const GREETINGS =
+  '할렐루야!\n소리노을의 찬양을 통해 많은 사람들이 위로받고 주님께 돌아오는 일들이 있기를 예수님의 이름으로 축복합니다. 교회학교 소리노을 찬양팀을 사랑하고 축복합니다.';
+
+export const PASTOR = '이영훈 목사';
+
+export const POSITION = '(여의도순복음교회 담임)';
