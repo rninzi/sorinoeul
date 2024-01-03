8b330974216947311038d01e09ea2d10a09b0f20
mywhzy <cmj2393a@naver.com>
2024-01-03 14:26:23 +0900

    feat: App에 헤더, 메인페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import styled from 'styled-components';
+import { Header } from './components/Header';
+import { MainPage } from './pages/MainPage';
+
 const App = () => {
-  return <></>;
+  return (
+    <Container>
+      <Header />
+      <MainPage />
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background: #f8f8f9;
+`;
 
 export default App;
