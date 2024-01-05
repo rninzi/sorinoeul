2124085f6b9e6e20e4f3103ce095ae6edf06f736
mywhzy <cmj2393a@naver.com>
2024-01-05 10:49:24 +0900

    feat: TrackListBox 컴포넌트 구현

---
+++ src/components/main/TrackListBox.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+import albumArt from '../../assets/images/album-art.png';
+import { TrackListItem } from './TrackListItem';
+import { mockupData } from '../../assets/data/mokupData';
+
+export const TrackListBox = () => {
+  return (
+    <Container>
+      <AlbumArtContainer>
+        <AlbumArtImg src={albumArt} />
+      </AlbumArtContainer>
+      <TrackList>
+        {mockupData.map((data) => (
+          <TrackListItem track={data} />
+        ))}
+      </TrackList>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 105px;
+  align-items: center;
+`;
+
+const AlbumArtContainer = styled.div`
+  max-width: 300px;
+  height: 300px;
+  background: #fff;
+  border-radius: 30px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset;
+  margin-bottom: 49px;
+`;
+
+const AlbumArtImg = styled.img`
+  width: 90%;
+  margin: 15px;
+`;
+
+const TrackList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
