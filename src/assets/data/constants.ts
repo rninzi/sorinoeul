33653627ffadb0f831814abd399ba58ac0450143
mywhzy <cmj2393a@naver.com>
2024-01-05 15:34:42 +0900

    feat: 메인페이지 내 타이틀 및 앨범 크레딧 정보 상수 추가

--- src/assets/data/constants.ts
+++ src/assets/data/constants.ts
@@ -10,3 +10,102 @@ export const ALBUM_INTRODUCTION =
   "소리노을은 1984년부터 지금까지 여의도순복음교회 교회학교 고등부 예배를 섬기고 있는 찬양팀입니다.\n첫 앨범 '내 안에 새겨진'은 다음 세대인 청소년들이 찬양을 통해 하나님을 더 깊이 만나고 더 기쁘게 예배하기를 소망하는 마음을 담았습니다. 앨범의 찬양들이 어떠한 예배의 자리에서도 우리 안에 새겨진 주님의 사랑을 다시 기억하게 하고 우리 모두가 하나님께서 찾으시는 예배자로 결단하는 은혜로 이어지기를 간절히 소망합니다.";
 
 export const COPYRIGHT = 'ⓒ 2023 SORINOEUL. ALL RIGHT RESERVED.';
+
+export const TITLE = {
+  albumCredit: 'ALBUM CREDIT',
+  apply: '소리노을 지원하기',
+  streaming: '스트리밍 사이트 바로가기',
+  serviceInfo: '고등부 예배 안내',
+};
+
+export const ALBUM_CREDIT = [
+  {
+    role: 'Executive Producer',
+    names: ['박진구'],
+  },
+  {
+    role: 'Project Supervisor',
+    names: ['양희경'],
+  },
+  {
+    role: 'Music Director',
+    names: ['이지혜', '최현경', '김소린'],
+  },
+  {
+    role: 'Vocal Director & Chorus Arrangement',
+    names: ['이지혜', '최현경', '김소린', '양희경'],
+  },
+  {
+    role: 'E.Guitar',
+    names: ['이삭', '최현규'],
+  },
+  {
+    role: 'A.Guitar',
+    names: ['정이레'],
+  },
+  {
+    role: 'E.Bass',
+    names: ['이정훈', '안광현', '황지성'],
+  },
+  {
+    role: 'Drums',
+    names: ['신연태', '김승연'],
+  },
+  {
+    role: 'Piano',
+    names: ['이지혜', '최현경', '김소린'],
+  },
+  {
+    role: 'Keyboard',
+    names: ['이지혜', '최현경', '김소린', '안예원'],
+  },
+  {
+    role: 'Vocals',
+    names: ['양희경', '윤소정', '고동환', '김예은', '여건희', '도현희'],
+  },
+  {
+    role: 'Chorus',
+    names: [
+      '양희경',
+      '고동환',
+      '여건희',
+      '도현희',
+      '도소희',
+      '김현종',
+      '여환희',
+      '김소린',
+      '남명준',
+      '김주영',
+      '유이레',
+      '황주영',
+    ],
+  },
+  {
+    role: 'Kids Chorus',
+    names: ['박성민', '박소율', '장하준', '장하임'],
+  },
+  {
+    role: 'String & Brass programming',
+    names: ['by Room10 해빈,준경'],
+  },
+  {
+    role: 'Recording Director',
+    names: ['오경환 @Sorinoeul Studio'],
+  },
+  {
+    role: 'Recorded Engineer',
+    names: ['김택진 @Sorinoeul Studio'],
+  },
+  {
+    role: 'Mixed',
+    names: ['by 장영환 @Sorinoeul Studio'],
+  },
+  {
+    role: 'Mastered',
+    names: ['by 박정언 @Sorinoeul Studio'],
+  },
+  {
+    role: 'Art Work & Design',
+    names: ['김소린'],
+  },
+];
