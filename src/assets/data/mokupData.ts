a3ba4d5f0d68e1cd8e318e0dafbaccf0fffed66f
mywhzy <cmj2393a@naver.com>
2024-01-05 10:48:38 +0900

    feat: Track List 임시 데이터 생성

---
+++ src/assets/data/mokupData.ts
@@ -0,0 +1,140 @@
+export const mockupData = [
+  {
+    id: 1,
+    title: '내 안에 새겨진',
+    vocal: '여건희',
+    songWriter: '여건희',
+    originalSong: undefined,
+    arrangers: ['최현경', '신연태'],
+    soundTrackLink:
+      'https://drive.google.com/file/d/1-CVojoo-4WkXBWRh-XjhK5a0kg5vdqcR/view?usp=sharing',
+    scoreLink:
+      'https://drive.google.com/file/d/11AMY2zv5rIq330sxpFD_U74OfVGmRPWc/view?usp=sharing',
+  },
+  {
+    id: 2,
+    title: '사랑의 주님 (부제-임마누엘)',
+    vocal: '고동환',
+    songWriter: '고동환',
+    originalSong: undefined,
+    arrangers: ['이지혜'],
+    soundTrackLink:
+      'https://drive.google.com/file/d/1-HSN1b5tl9gP2FedCp6fQ4pofZ8q0MvQ/view?usp=sharing',
+    scoreLink:
+      'https://drive.google.com/file/d/110xzqn6HbvRYOOAvQXF3jYyW03ttQ2Pz/view?usp=sharing',
+  },
+  {
+    id: 3,
+    title: '아 하나님의 은혜로',
+    vocal: '김예은',
+    songWriter: undefined,
+    originalSong: '찬송가 310장',
+    arrangers: ['최현경', '신연태'],
+    soundTrackLink:
+      'https://drive.google.com/file/d/1-UCvYa6nRc3WI7xHfdoEI5AIH8z2UKHX/view?usp=sharing',
+    scoreLink:
+      'https://drive.google.com/file/d/10hywnntRGH3n7flDr5cJK2msvK2F4-Z6/view?usp=sharing',
+  },
+  {
+    id: 4,
+    title: '예수님 때문에',
+    vocal: '도현희',
+    songWriter: '김도훈',
+    originalSong: undefined,
+    arrangers: ['최현경', '최현규', '신연태'],
+    soundTrackLink:
+      'https://drive.google.com/file/d/1-RlORIw_EncSTXGeRnRRz2Bu_oooSUxZ/view?usp=sharing',
+    scoreLink:
+      'https://drive.google.com/file/d/10acgb9xlH3CKpUXQtJYQz5wuAGifqneN/view?usp=sharing',
+  },
+  {
+    id: 5,
+    title: '생명나무 (부제-한나의 기도)',
+    vocal: '고동환',
+    songWriter: '고동환',
+    originalSong: undefined,
+    arrangers: ['이지혜'],
+    soundTrackLink:
+      'https://drive.google.com/file/d/1-X8cZ4JNy9zPk6OcHf4QwVEQAEhZ4GBr/view?usp=sharing',
+    scoreLink:
+      'https://drive.google.com/file/d/10_IPXNqTkUzgCJU9cl605tPKwurqbh-S/view?usp=sharing',
+  },
+  {
+    id: 6,
+    title: '주를 향한 그리움',
+    vocal: '양희경',
+    songWriter: '김도훈',
+    originalSong: undefined,
+    arrangers: ['이지혜'],
+    soundTrackLink:
+      'https://drive.google.com/file/d/1-Wlh1JmqAXy0M_kVJ72GGDixLtELwYuE/view?usp=sharing',
+    scoreLink:
+      'https://drive.google.com/file/d/10ZvlX3N4BbYx7ZeAhlz-yWODXmiCCodS/view?usp=sharing',
+  },
+  {
+    id: 7,
+    title: '내 안에 새겨진(Inst.)',
+    vocal: undefined,
+    songWriter: undefined,
+    originalSong: undefined,
+    arrangers: undefined,
+    soundTrackLink:
+      'https://drive.google.com/file/d/1-uRkofSEQJCYaKzp2Kw8ZkvrwYawRTo9/view?usp=sharing',
+    scoreLink: undefined,
+  },
+  {
+    id: 8,
+    title: '사랑의 주님(Inst.)',
+    vocal: undefined,
+    songWriter: undefined,
+    originalSong: undefined,
+    arrangers: undefined,
+    soundTrackLink:
+      'https://drive.google.com/file/d/1-XXujHXQySblCfDZNQrMUYoRCrEJdDFw/view?usp=sharing',
+    scoreLink: undefined,
+  },
+  {
+    id: 9,
+    title: '아 하나님의 은혜로(Inst.)',
+    vocal: undefined,
+    songWriter: undefined,
+    originalSong: undefined,
+    arrangers: undefined,
+    soundTrackLink:
+      'https://drive.google.com/file/d/10DqpxAitVaxBo6lfFoO1COG4vd-0t4ta/view?usp=sharing',
+    scoreLink: undefined,
+  },
+  {
+    id: 10,
+    title: '예수님 때문에(Inst.)',
+    vocal: undefined,
+    songWriter: undefined,
+    originalSong: undefined,
+    arrangers: undefined,
+    soundTrackLink:
+      'https://drive.google.com/file/d/107tjFGPDdeqJD7hF1kLdu0E_3MbdyYUM/view?usp=sharing',
+    scoreLink: undefined,
+  },
+  {
+    id: 11,
+    title: '생명나무(Inst.)',
+    vocal: undefined,
+    songWriter: undefined,
+    originalSong: undefined,
+    arrangers: undefined,
+    soundTrackLink:
+      'https://drive.google.com/file/d/10JWSibzIrfShl_t9IyGcybekvY1yNY28/view?usp=sharing',
+    scoreLink: undefined,
+  },
+  {
+    id: 12,
+    title: '주를 향한 그리움(Inst.)',
+    vocal: undefined,
+    songWriter: undefined,
+    originalSong: undefined,
+    arrangers: undefined,
+    soundTrackLink:
+      'https://drive.google.com/file/d/1-GJiG9NUex0_X0L9tmybCZ6N-L1OK52v/view?usp=sharing',
+    scoreLink: undefined,
+  },
+];
