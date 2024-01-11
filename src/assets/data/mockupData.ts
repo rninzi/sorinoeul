b071391b00fb7b25956e614b04d35966ac7deb24
mywhzy <cmj2393a@naver.com>
2024-01-11 14:58:49 +0900

    feat: track list 음원, 악보 url 변경

--- src/assets/data/mockupData.ts
+++ src/assets/data/mockupData.ts
@@ -7,9 +7,9 @@ export const mockupData = [
     originalSong: undefined,
     arrangers: ['최현경', '신연태'],
     soundTrackLink:
-      'https://drive.google.com/file/d/1-CVojoo-4WkXBWRh-XjhK5a0kg5vdqcR/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F1.%E1%84%82%E1%85%A2%20%E1%84%8B%E1%85%A1%E1%86%AB%E1%84%8B%E1%85%A6%20%E1%84%89%E1%85%A2%E1%84%80%E1%85%A7%E1%84%8C%E1%85%B5%E1%86%AB.mp3?alt=media&token=bc0dbce7-68c8-4953-ab25-1dcfb453c7aa',
     scoreLink:
-      'https://drive.google.com/file/d/11AMY2zv5rIq330sxpFD_U74OfVGmRPWc/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/score%2F1.%E1%84%82%E1%85%A2%20%E1%84%8B%E1%85%A1%E1%86%AB%E1%84%8B%E1%85%A6%20%E1%84%89%E1%85%A2%E1%84%80%E1%85%A7%E1%84%8C%E1%85%B5%E1%86%AB.pdf?alt=media&token=2d5293f4-da38-4aa1-b79d-90ed598be92a',
   },
   {
     id: 2,
@@ -19,9 +19,9 @@ export const mockupData = [
     originalSong: undefined,
     arrangers: ['이지혜'],
     soundTrackLink:
-      'https://drive.google.com/file/d/1-HSN1b5tl9gP2FedCp6fQ4pofZ8q0MvQ/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F2.%E1%84%89%E1%85%A1%E1%84%85%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%B4%20%E1%84%8C%E1%85%AE%E1%84%82%E1%85%B5%E1%86%B7.mp3?alt=media&token=34800c1c-504d-4a34-a168-7bfd0a7f7f30',
     scoreLink:
-      'https://drive.google.com/file/d/110xzqn6HbvRYOOAvQXF3jYyW03ttQ2Pz/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/score%2F2.%E1%84%89%E1%85%A1%E1%84%85%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%B4%20%E1%84%8C%E1%85%AE%E1%84%82%E1%85%B5%E1%86%B7.pdf?alt=media&token=3e0cad34-42a8-47ab-ad77-8d3aac674bdf',
   },
   {
     id: 3,
@@ -31,9 +31,9 @@ export const mockupData = [
     originalSong: '찬송가 310장',
     arrangers: ['최현경', '신연태'],
     soundTrackLink:
-      'https://drive.google.com/file/d/1-UCvYa6nRc3WI7xHfdoEI5AIH8z2UKHX/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F3.%E1%84%8B%E1%85%A1%20%E1%84%92%E1%85%A1%E1%84%82%E1%85%A1%E1%84%82%E1%85%B5%E1%86%B7%E1%84%8B%E1%85%B4%20%E1%84%8B%E1%85%B3%E1%86%AB%E1%84%92%E1%85%A8%E1%84%85%E1%85%A9.mp3?alt=media&token=851e8a16-778b-49d9-abe3-d1e02230ac9f',
     scoreLink:
-      'https://drive.google.com/file/d/10hywnntRGH3n7flDr5cJK2msvK2F4-Z6/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/score%2F3.%E1%84%8B%E1%85%A1%20%E1%84%92%E1%85%A1%E1%84%82%E1%85%A1%E1%84%82%E1%85%B5%E1%86%B7%E1%84%8B%E1%85%B4%20%E1%84%8B%E1%85%B3%E1%86%AB%E1%84%92%E1%85%A8%E1%84%85%E1%85%A9.pdf?alt=media&token=4be34f23-ac72-45b3-85c8-57010b4011d5',
   },
   {
     id: 4,
@@ -43,9 +43,9 @@ export const mockupData = [
     originalSong: undefined,
     arrangers: ['최현경', '최현규', '신연태'],
     soundTrackLink:
-      'https://drive.google.com/file/d/1-RlORIw_EncSTXGeRnRRz2Bu_oooSUxZ/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F4.%E1%84%8B%E1%85%A8%E1%84%89%E1%85%AE%E1%84%82%E1%85%B5%E1%86%B7%20%E1%84%84%E1%85%A2%E1%84%86%E1%85%AE%E1%86%AB%E1%84%8B%E1%85%A6.mp3?alt=media&token=0887dc19-b1c6-4b54-954b-17330e3c247a',
     scoreLink:
-      'https://drive.google.com/file/d/10acgb9xlH3CKpUXQtJYQz5wuAGifqneN/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/score%2F4.%E1%84%8B%E1%85%A8%E1%84%89%E1%85%AE%E1%84%82%E1%85%B5%E1%86%B7%20%E1%84%84%E1%85%A2%E1%84%86%E1%85%AE%E1%86%AB%E1%84%8B%E1%85%A6.pdf?alt=media&token=40f75916-154e-465e-9137-3ec8413c0265',
   },
   {
     id: 5,
@@ -55,9 +55,9 @@ export const mockupData = [
     originalSong: undefined,
     arrangers: ['이지혜'],
     soundTrackLink:
-      'https://drive.google.com/file/d/1-X8cZ4JNy9zPk6OcHf4QwVEQAEhZ4GBr/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F5.%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC%E1%84%82%E1%85%A1%E1%84%86%E1%85%AE.mp3?alt=media&token=a7ce079f-255b-421c-bbb4-46ff9ada7eb4',
     scoreLink:
-      'https://drive.google.com/file/d/10_IPXNqTkUzgCJU9cl605tPKwurqbh-S/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/score%2F5.%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC%E1%84%82%E1%85%A1%E1%84%86%E1%85%AE.pdf?alt=media&token=b5bd0644-c765-4313-a4f7-041517df58a8',
   },
   {
     id: 6,
@@ -67,9 +67,9 @@ export const mockupData = [
     originalSong: undefined,
     arrangers: ['이지혜'],
     soundTrackLink:
-      'https://drive.google.com/file/d/1-Wlh1JmqAXy0M_kVJ72GGDixLtELwYuE/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F6.%E1%84%8C%E1%85%AE%E1%84%85%E1%85%B3%E1%86%AF%20%E1%84%92%E1%85%A3%E1%86%BC%E1%84%92%E1%85%A1%E1%86%AB%20%E1%84%80%E1%85%B3%E1%84%85%E1%85%B5%E1%84%8B%E1%85%AE%E1%86%B7.mp3?alt=media&token=0077eb94-ca10-4d07-8d15-f593a19167c0',
     scoreLink:
-      'https://drive.google.com/file/d/10ZvlX3N4BbYx7ZeAhlz-yWODXmiCCodS/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/score%2F6.%E1%84%8C%E1%85%AE%E1%84%85%E1%85%B3%E1%86%AF%20%E1%84%92%E1%85%A3%E1%86%BC%E1%84%92%E1%85%A1%E1%86%AB%20%E1%84%80%E1%85%B3%E1%84%85%E1%85%B5%E1%84%8B%E1%85%AE%E1%86%B7.pdf?alt=media&token=81a20fd0-cdd9-4913-8753-a4abca620609',
   },
   {
     id: 7,
@@ -79,7 +79,7 @@ export const mockupData = [
     originalSong: undefined,
     arrangers: undefined,
     soundTrackLink:
-      'https://drive.google.com/file/d/1-uRkofSEQJCYaKzp2Kw8ZkvrwYawRTo9/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F7.%E1%84%82%E1%85%A2%20%E1%84%8B%E1%85%A1%E1%86%AB%E1%84%8B%E1%85%A6%20%E1%84%89%E1%85%A2%E1%84%80%E1%85%A7%E1%84%8C%E1%85%B5%E1%86%AB(Inst.).mp3?alt=media&token=38741450-b12a-4195-9cd3-5d30e4c9ffbb',
     scoreLink: undefined,
   },
   {
@@ -90,7 +90,7 @@ export const mockupData = [
     originalSong: undefined,
     arrangers: undefined,
     soundTrackLink:
-      'https://drive.google.com/file/d/1-XXujHXQySblCfDZNQrMUYoRCrEJdDFw/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F8.%E1%84%89%E1%85%A1%E1%84%85%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%B4%20%E1%84%8C%E1%85%AE%E1%84%82%E1%85%B5%E1%86%B7(Inst.).mp3?alt=media&token=13432434-4215-46c1-80b3-04d254b1eeeb',
     scoreLink: undefined,
   },
   {
@@ -101,7 +101,7 @@ export const mockupData = [
     originalSong: undefined,
     arrangers: undefined,
     soundTrackLink:
-      'https://drive.google.com/file/d/10DqpxAitVaxBo6lfFoO1COG4vd-0t4ta/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F9.%E1%84%8B%E1%85%A1%20%E1%84%92%E1%85%A1%E1%84%82%E1%85%A1%E1%84%82%E1%85%B5%E1%86%B7%E1%84%8B%E1%85%B4%20%E1%84%8B%E1%85%B3%E1%86%AB%E1%84%92%E1%85%A8%E1%84%85%E1%85%A9(Inst.).mp3?alt=media&token=7357e1dc-143d-482b-8d46-740b0b951e24',
     scoreLink: undefined,
   },
   {
@@ -112,7 +112,7 @@ export const mockupData = [
     originalSong: undefined,
     arrangers: undefined,
     soundTrackLink:
-      'https://drive.google.com/file/d/107tjFGPDdeqJD7hF1kLdu0E_3MbdyYUM/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F10.%E1%84%8B%E1%85%A8%E1%84%89%E1%85%AE%E1%84%82%E1%85%B5%E1%86%B7%20%E1%84%84%E1%85%A2%E1%84%86%E1%85%AE%E1%86%AB%E1%84%8B%E1%85%A6(Inst.).mp3?alt=media&token=732825d8-a0c4-4e42-b8cf-3e2502345aa3',
     scoreLink: undefined,
   },
   {
@@ -123,7 +123,7 @@ export const mockupData = [
     originalSong: undefined,
     arrangers: undefined,
     soundTrackLink:
-      'https://drive.google.com/file/d/10JWSibzIrfShl_t9IyGcybekvY1yNY28/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F11.%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC%E1%84%82%E1%85%A1%E1%84%86%E1%85%AE(Inst.).mp3?alt=media&token=d134529a-ed0c-4565-b54d-1dae819bd384',
     scoreLink: undefined,
   },
   {
@@ -134,7 +134,7 @@ export const mockupData = [
     originalSong: undefined,
     arrangers: undefined,
     soundTrackLink:
-      'https://drive.google.com/file/d/1-GJiG9NUex0_X0L9tmybCZ6N-L1OK52v/view?usp=sharing',
+      'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F12.%E1%84%8C%E1%85%AE%E1%84%85%E1%85%B3%E1%86%AF%20%E1%84%92%E1%85%A3%E1%86%BC%E1%84%92%E1%85%A1%E1%86%AB%20%E1%84%80%E1%85%B3%E1%84%85%E1%85%B5%E1%84%8B%E1%85%AE%E1%86%B7(Inst.).mp3?alt=media&token=b8015608-f1ef-4385-86ae-e7a6006fa20b',
     scoreLink: undefined,
   },
 ];
