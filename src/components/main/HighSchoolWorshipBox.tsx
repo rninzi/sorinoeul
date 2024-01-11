e1677ac31a865de73ea7cb6424c7088a06dde0d8
mywhzy <cmj2393a@naver.com>
2024-01-11 13:12:12 +0900

    feat: HighSchoolWorshipBox 컴포넌트 구현

---
+++ src/components/main/HighSchoolWorshipBox.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import { TitleBox } from '../TitleBox';
+import { TITLE, WORSHIP_INFO } from '../../assets/data/constants';
+import { WorshipInfoBox } from './WorshipInfoBox';
+
+export const HighSchoolWorshipBox = () => {
+  return (
+    <Container>
+      <TitleBox>{TITLE.worshipInfo}</TitleBox>
+      <InnerContainer>
+        {WORSHIP_INFO.map((item) => (
+          <WorshipInfoBox
+            imageSrc={item.imgSrc}
+            target={item.target}
+            place={item.place}
+            time={item.time}
+            youtubeID={item.youtubeID}
+            youtubeURL={item.youtubeURL}
+          />
+        ))}
+      </InnerContainer>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: calc(100% - 30px);
+  display: flex;
+  flex-direction: column;
+  margin: 105px 0 90px 30px;
+`;
+
+const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  margin-top: 30px;
+`;
