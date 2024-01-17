16e1514c999ac966947cb2d219c17636beb8b577
mywhzy <cmj2393a@naver.com>
2024-01-17 19:43:12 +0900

    feat: 페이지 Route 추가 feat: 헤더 onClick 함수 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,26 @@ import styled from 'styled-components';
 import { Header } from 'components/Header';
 import { MainPage } from 'pages/MainPage';
 import { Footer } from 'components/Footer';
+import { ScorePage } from 'pages/ScorePage';
+import { Routes, Route } from 'react-router-dom';
+import { scrollToTop } from 'utils/scrollToTop';
+import { useNavigate } from 'react-router-dom';
 
 const App = () => {
+  const navigate = useNavigate();
+
+  const onLogoClick = () => {
+    scrollToTop();
+    navigate('/');
+  };
+
   return (
     <Container>
-      <Header />
-      <MainPage />
+      <Header onLogoClick={onLogoClick} />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/score/:trackNumber" element={<ScorePage />} />
+      </Routes>
       <Footer />
     </Container>
   );
