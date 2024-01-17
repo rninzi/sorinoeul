b99fe9993fa9c3592e4537f82f18a77be9012663
mywhzy <cmj2393a@naver.com>
2024-01-17 14:28:38 +0900

    feat: 데이터에 악보 이미지 추가 feat: title과 subtitle 구분되게 변경

--- src/assets/data/mockupData.ts
+++ src/assets/data/mockupData.ts
@@ -1,3 +1,15 @@
+import track1_01 from 'assets/images/scores/track1_01.jpg';
+import track1_02 from 'assets/images/scores/track1_02.jpg';
+import track2_01 from 'assets/images/scores/track2_01.jpg';
+import track2_02 from 'assets/images/scores/track2_02.jpg';
+import track3_01 from 'assets/images/scores/track3_01.jpg';
+import track3_02 from 'assets/images/scores/track3_02.jpg';
+import track3_03 from 'assets/images/scores/track3_03.jpg';
+import track4 from 'assets/images/scores/track4.jpg';
+import track5 from 'assets/images/scores/track5.jpg';
+import track6_01 from 'assets/images/scores/track6_01.jpg';
+import track6_02 from 'assets/images/scores/track6_02.jpg';
+
 export const soundtrackCreditData = [
   {
     vocal: ['여건희'],
@@ -210,15 +222,18 @@ export const trackListData = [
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F1.%E1%84%82%E1%85%A2%20%E1%84%8B%E1%85%A1%E1%86%AB%E1%84%8B%E1%85%A6%20%E1%84%89%E1%85%A2%E1%84%80%E1%85%A7%E1%84%8C%E1%85%B5%E1%86%AB.mp3?alt=media&token=bc0dbce7-68c8-4953-ab25-1dcfb453c7aa',
     scoreURL:
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/score%2F1.%E1%84%82%E1%85%A2%20%E1%84%8B%E1%85%A1%E1%86%AB%E1%84%8B%E1%85%A6%20%E1%84%89%E1%85%A2%E1%84%80%E1%85%A7%E1%84%8C%E1%85%B5%E1%86%AB.pdf?alt=media&token=2d5293f4-da38-4aa1-b79d-90ed598be92a',
+    scores: [track1_01, track1_02],
   },
   {
     trackNumber: 2,
-    title: '사랑의 주님 (부제-임마누엘)',
+    title: '사랑의 주님',
+    subtitle: '(부제-임마누엘)',
     credit: soundtrackCreditData[1],
     soundTrackURL:
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F2.%E1%84%89%E1%85%A1%E1%84%85%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%B4%20%E1%84%8C%E1%85%AE%E1%84%82%E1%85%B5%E1%86%B7.mp3?alt=media&token=34800c1c-504d-4a34-a168-7bfd0a7f7f30',
     scoreURL:
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/score%2F2.%E1%84%89%E1%85%A1%E1%84%85%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%B4%20%E1%84%8C%E1%85%AE%E1%84%82%E1%85%B5%E1%86%B7.pdf?alt=media&token=3e0cad34-42a8-47ab-ad77-8d3aac674bdf',
+    scores: [track2_01, track2_02],
   },
   {
     trackNumber: 3,
@@ -228,6 +243,7 @@ export const trackListData = [
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F3.%E1%84%8B%E1%85%A1%20%E1%84%92%E1%85%A1%E1%84%82%E1%85%A1%E1%84%82%E1%85%B5%E1%86%B7%E1%84%8B%E1%85%B4%20%E1%84%8B%E1%85%B3%E1%86%AB%E1%84%92%E1%85%A8%E1%84%85%E1%85%A9.mp3?alt=media&token=851e8a16-778b-49d9-abe3-d1e02230ac9f',
     scoreURL:
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/score%2F3.%E1%84%8B%E1%85%A1%20%E1%84%92%E1%85%A1%E1%84%82%E1%85%A1%E1%84%82%E1%85%B5%E1%86%B7%E1%84%8B%E1%85%B4%20%E1%84%8B%E1%85%B3%E1%86%AB%E1%84%92%E1%85%A8%E1%84%85%E1%85%A9.pdf?alt=media&token=4be34f23-ac72-45b3-85c8-57010b4011d5',
+    scores: [track3_01, track3_02, track3_03],
   },
   {
     trackNumber: 4,
@@ -237,15 +253,18 @@ export const trackListData = [
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F4.%E1%84%8B%E1%85%A8%E1%84%89%E1%85%AE%E1%84%82%E1%85%B5%E1%86%B7%20%E1%84%84%E1%85%A2%E1%84%86%E1%85%AE%E1%86%AB%E1%84%8B%E1%85%A6.mp3?alt=media&token=0887dc19-b1c6-4b54-954b-17330e3c247a',
     scoreURL:
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/score%2F4.%E1%84%8B%E1%85%A8%E1%84%89%E1%85%AE%E1%84%82%E1%85%B5%E1%86%B7%20%E1%84%84%E1%85%A2%E1%84%86%E1%85%AE%E1%86%AB%E1%84%8B%E1%85%A6.pdf?alt=media&token=40f75916-154e-465e-9137-3ec8413c0265',
+    scores: [track4],
   },
   {
     trackNumber: 5,
-    title: '생명나무 (부제-한나의 기도)',
+    title: '생명나무',
+    subtitle: '(부제-한나의 기도)',
     credit: soundtrackCreditData[4],
     soundTrackURL:
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F5.%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC%E1%84%82%E1%85%A1%E1%84%86%E1%85%AE.mp3?alt=media&token=a7ce079f-255b-421c-bbb4-46ff9ada7eb4',
     scoreURL:
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/score%2F5.%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC%E1%84%82%E1%85%A1%E1%84%86%E1%85%AE.pdf?alt=media&token=b5bd0644-c765-4313-a4f7-041517df58a8',
+    scores: [track5],
   },
   {
     trackNumber: 6,
@@ -255,6 +274,7 @@ export const trackListData = [
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F6.%E1%84%8C%E1%85%AE%E1%84%85%E1%85%B3%E1%86%AF%20%E1%84%92%E1%85%A3%E1%86%BC%E1%84%92%E1%85%A1%E1%86%AB%20%E1%84%80%E1%85%B3%E1%84%85%E1%85%B5%E1%84%8B%E1%85%AE%E1%86%B7.mp3?alt=media&token=0077eb94-ca10-4d07-8d15-f593a19167c0',
     scoreURL:
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/score%2F6.%E1%84%8C%E1%85%AE%E1%84%85%E1%85%B3%E1%86%AF%20%E1%84%92%E1%85%A3%E1%86%BC%E1%84%92%E1%85%A1%E1%86%AB%20%E1%84%80%E1%85%B3%E1%84%85%E1%85%B5%E1%84%8B%E1%85%AE%E1%86%B7.pdf?alt=media&token=81a20fd0-cdd9-4913-8753-a4abca620609',
+    scores: [track6_01, track6_02],
   },
   {
     trackNumber: 7,
@@ -263,6 +283,7 @@ export const trackListData = [
     soundTrackURL:
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F7.%E1%84%82%E1%85%A2%20%E1%84%8B%E1%85%A1%E1%86%AB%E1%84%8B%E1%85%A6%20%E1%84%89%E1%85%A2%E1%84%80%E1%85%A7%E1%84%8C%E1%85%B5%E1%86%AB(Inst.).mp3?alt=media&token=38741450-b12a-4195-9cd3-5d30e4c9ffbb',
     scoreURL: null,
+    scores: null,
   },
   {
     trackNumber: 8,
@@ -271,6 +292,7 @@ export const trackListData = [
     soundTrackURL:
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F8.%E1%84%89%E1%85%A1%E1%84%85%E1%85%A1%E1%86%BC%E1%84%8B%E1%85%B4%20%E1%84%8C%E1%85%AE%E1%84%82%E1%85%B5%E1%86%B7(Inst.).mp3?alt=media&token=13432434-4215-46c1-80b3-04d254b1eeeb',
     scoreURL: null,
+    scores: null,
   },
   {
     trackNumber: 9,
@@ -279,6 +301,7 @@ export const trackListData = [
     soundTrackURL:
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F9.%E1%84%8B%E1%85%A1%20%E1%84%92%E1%85%A1%E1%84%82%E1%85%A1%E1%84%82%E1%85%B5%E1%86%B7%E1%84%8B%E1%85%B4%20%E1%84%8B%E1%85%B3%E1%86%AB%E1%84%92%E1%85%A8%E1%84%85%E1%85%A9(Inst.).mp3?alt=media&token=7357e1dc-143d-482b-8d46-740b0b951e24',
     scoreURL: null,
+    scores: null,
   },
   {
     trackNumber: 10,
@@ -287,6 +310,7 @@ export const trackListData = [
     soundTrackURL:
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F10.%E1%84%8B%E1%85%A8%E1%84%89%E1%85%AE%E1%84%82%E1%85%B5%E1%86%B7%20%E1%84%84%E1%85%A2%E1%84%86%E1%85%AE%E1%86%AB%E1%84%8B%E1%85%A6(Inst.).mp3?alt=media&token=732825d8-a0c4-4e42-b8cf-3e2502345aa3',
     scoreURL: null,
+    scores: null,
   },
   {
     trackNumber: 11,
@@ -295,6 +319,7 @@ export const trackListData = [
     soundTrackURL:
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F11.%E1%84%89%E1%85%A2%E1%86%BC%E1%84%86%E1%85%A7%E1%86%BC%E1%84%82%E1%85%A1%E1%84%86%E1%85%AE(Inst.).mp3?alt=media&token=d134529a-ed0c-4565-b54d-1dae819bd384',
     scoreURL: null,
+    scores: null,
   },
   {
     trackNumber: 12,
@@ -303,5 +328,6 @@ export const trackListData = [
     soundTrackURL:
       'https://firebasestorage.googleapis.com/v0/b/sorinoeul-yfgc.appspot.com/o/soundtrack%2F12.%E1%84%8C%E1%85%AE%E1%84%85%E1%85%B3%E1%86%AF%20%E1%84%92%E1%85%A3%E1%86%BC%E1%84%92%E1%85%A1%E1%86%AB%20%E1%84%80%E1%85%B3%E1%84%85%E1%85%B5%E1%84%8B%E1%85%AE%E1%86%B7(Inst.).mp3?alt=media&token=b8015608-f1ef-4385-86ae-e7a6006fa20b',
     scoreURL: null,
+    scores: null,
   },
 ];
